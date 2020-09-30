cbac194554f51d3e83cb32b1f5cc810c3d6998d4
nickthorpe71 <nickthorpe71@gmail.com>
2020-09-30 16:34:14 -0500

    adds update and delete

--- src/resolvers/ScoreResolver.ts
+++ src/resolvers/ScoreResolver.ts
@@ -12,6 +12,7 @@ class ScoreInput {
 
 @Resolver()
 export class ScoreResolver {
+  // create
   @Mutation(() => Score) // Mutations are generally for updating or making changes to server
   async createScore(
     @Arg('options', () => ScoreInput) options: ScoreInput,
@@ -20,9 +21,28 @@ export class ScoreResolver {
     return score;
   }
 
+  // read
   @Query(() => [Score])
   scores() {
     return Score.find()
+  }
+
+  // update
+  @Mutation(() => Boolean)
+  async updateScore(
+    @Arg('id', () => Int) id: number,
+    @Arg('input', () => ScoreInput) input: ScoreInput
+  ) {
+    await Score.update({ id }, input);
+    return true;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteScore(
+    @Arg('id', () => Int) id: number
+  ) {
+    await Score.delete({ id });
+    return true;
   }
 }
 
